653295aa3c2b479ef897d678788c9e7af61e6d9f
brittanymae01 <brittanymae01>
2019-10-16 18:45:50 -0700

    changes I made after 1 on 1

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -65,30 +65,25 @@ console.log(fullNames);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. Populate an array called `firstNamesAllCaps`. This array will contain just strings.
-let firstNamesAllCaps = [];
-runners.map(function (currentValue) {
-  firstNamesAllCaps.push(currentValue.first_name.toUpperCase());
+let firstNamesAllCaps = runners.map(function (currentValue) {
+  return currentValue.first_name.toUpperCase();
 });
 console.log(firstNamesAllCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
-let runnersLargeSizeShirt = [];
-let shirtFilter = runners.filter(function (currentValue) {
+let runnersLargeSizeShirt = runners.filter(function (currentValue) {
   return currentValue.shirt_size === "L";
 });
 //why was I not able to add the filter method to the string of runnersLargeSize Shirt?
-runnersLargeSizeShirt.push(shirtFilter);
 console.log(runnersLargeSizeShirt);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations and save the total into a ticketPriceTotal variable.
-let ticketPriceTotal = 0;
-let ticketReduce = runners.reduce(function (accumulator, currentValue) {
+let ticketPriceTotal = runners.reduce(function (accumulator, currentValue) {
   return accumulator + currentValue.donation;
 }, 0);
 //same question from previous challenge???
-ticketPriceTotal = ticketReduce;
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
